e7717a1fe4731a43025001e0568428e20b003cf2
wangzhaohan2910 <158981244+wangzhaohan2910@users.noreply.github.com>
2025-09-05 13:53:50 +0800

    Create matrix.cpp

---
+++ matrix.cpp
@@ -0,0 +1,31 @@
+int p;
+template <int a, int b>
+struct matrix: array<array<int, b>, a>
+{
+	template <int c>
+	matrix<a, c> operator*(const matrix<b, c>& rhs) const
+	{
+		matrix<a, c> res{};
+		for (int i{}; i < a; i++)
+			for (int j{}; j < b; j++)
+				for (int k{}; k < c; k++)
+                    if ((res[i][k] += (*this)[i][j] * rhs[j][k] % p) >= p)
+                        res[i][k] -= p;
+		return res;
+	}
+    matrix<a, b> pow(int n)
+    {
+        if constexpr (a == b)
+        {
+        	matrix<a, a> res{}, m{*this};
+        	for (int i{}; i < a; i++)
+        		res[i][i] = 1;
+        	for (; n; m = m * m, n >>= 1)
+        		if (n & 1)
+        			res = res * m;
+        	return res;
+        }
+        else
+            return *this;
+    }
+};

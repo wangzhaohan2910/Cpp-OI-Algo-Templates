cbd1776088b7c8ad6fe5e9eb2ec9420bf88dd8ca
wangzhaohan2910 <wangcf3000@sina.com>
2025-09-15 18:39:56 +0800

    matrix

--- matrix.cpp
+++ matrix.cpp
@@ -5,8 +5,11 @@ class matrix
     matrix(int h, int w)
 	: h{h}, w{w}, r{new int*[h]}
     {
-	for (int i{}; i < h; i++)
-	    r[i] = new int[w];
+	if (h && w)
+	    for (int i{}; i < h; i++)
+		r[i] = new int[w];
+	else
+	    r = nullptr;
     }
     ~matrix()
     {
@@ -14,17 +17,47 @@ class matrix
 	    delete[] r[i];
 	delete[] r;
     }
-    matrix(const matrix& rhs)
+    matrix(const matrix &rhs)
 	: h{rhs.h}, w{rhs.w}, r{new int*[h]}
     {
 	for (int i{}; i < h; i++)
 	    r[i] = new int[w],
 	    memcpy(r[i], rhs.r[i], w);
     }
-    matrix(matrix&& rhs)
+    matrix(matrix &&rhs)
 	: h{rhs.h}, w{rhs.w}, r{rhs.r}
     {
 	rhs.h = rhs.w = rhs.r = 0;
     }
-    matrix operator*(const matrix& )
+    matrix& operator=(const matrix &rhs)
+    {
+	for (int i{}; i < h; i++)
+	    delete[] r[i];
+	delete[] r;
+	h = rhs.h;
+	w = rhs.w;
+	r = new int*[h];
+	for (int i{}; i < h; i++)
+	    r[i] = new int[w],
+	    memcpy(r[i], rhs.r[i], w);
+	return *this;
+    }
+    matrix& operator=(matrix &&rhs)
+    {
+	for (int i{}; i < h; i++)
+	    delete[] r[i];
+	delete[] r;
+	h = rhs.h;
+	w = rhs.w;
+	r = rhs.r;
+	rhs.h = rhs.w = rhs.r = 0;
+    }
+    matrix operator*(const matrix &rhs)
+    {
+	if (w != rhs.h)
+	    return matrix(0, 0);
+	else
+	    // TODO
+    }
+    matrix pow(int k, /*TODO: The initial matrix*/);
 };
